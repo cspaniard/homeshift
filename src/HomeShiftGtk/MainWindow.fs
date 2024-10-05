65ee11bde31222fb778d5edebe21ce7aeadbf8bc
David Sanromá <dsanroma@gmail.com>
2024-10-06 01:47:30 +0200

    Primeras pruebas de Tool Buttons.

--- src/HomeShiftGtk/MainWindow.fs
+++ src/HomeShiftGtk/MainWindow.fs
@@ -13,6 +13,12 @@ type MainWindow(WindowIdName : string) as this =
     //------------------------------------------------------------------------------------------------------------------
 
     let MainLabel = this.Gui.GetObject("MainLabel") :?> Label
+    let CreateToolButton = this.Gui.GetObject("CreateToolButton") :?> ToolButton
+    let RestoreToolButton = this.Gui.GetObject("RestoreToolButton") :?> ToolButton
+    let DeleteToolButton = this.Gui.GetObject("DeleteToolButton") :?> ToolButton
+    let ExamineToolButton = this.Gui.GetObject("ExamineToolButton") :?> ToolButton
+    let ConfigureToolButton = this.Gui.GetObject("ConfigureToolButton") :?> ToolButton
+    let MenuToolButton = this.Gui.GetObject("MenuToolButton") :?> ToolButton
 
     let VM = MainWindowVM()
 
@@ -20,6 +26,12 @@ type MainWindow(WindowIdName : string) as this =
     // Inicializa el formulario.
     // -----------------------------------------------------------------------------------------------------------------
     do
+        CreateToolButton.Label <- "Crear"
+        RestoreToolButton.Label <- "Restaurar"
+        DeleteToolButton.Label <- "Eliminar"
+        ExamineToolButton.Label <- "Examinar"
+        ConfigureToolButton.Label <- "Configurar"
+        MenuToolButton.Label <- "Menú"
         // -------------------------------------------------------------------------------------------------------------
         // Prepara y muestra la ventana.
         // -------------------------------------------------------------------------------------------------------------
@@ -49,5 +61,23 @@ type MainWindow(WindowIdName : string) as this =
         Application.Quit()
     //------------------------------------------------------------------------------------------------------------------
 
-    member _.TestButtonClicked (_ : System.Object) (args : EventArgs) =
+    member _.TestButtonClicked (_ : System.Object) (_ : EventArgs) =
         MainLabel.Text <- "Hello, World!"
+
+    member _.CreateToolButtonClicked (_ : System.Object) (_ : EventArgs) =
+        MainLabel.Text <- "Creamos un nuevo snapshot."
+
+    member _.RestoreToolButtonClicked (_ : System.Object) (_ : EventArgs) =
+        MainLabel.Text <- "Restauramos un snapshot."
+
+    member _.DeleteToolButtonClicked (_ : System.Object) (_ : EventArgs) =
+        MainLabel.Text <- "Eliminamos un snapshot."
+
+    member _.ExamineToolButtonClicked (_ : System.Object) (_ : EventArgs) =
+        MainLabel.Text <- "Examinamos un snapshot."
+
+    member _.ConfigureToolButtonClicked (_ : System.Object) (_ : EventArgs) =
+        MainLabel.Text <- "Configuramos la aplicación."
+
+    member _.MenuToolButtonClicked (_ : System.Object) (_ : EventArgs) =
+        MainLabel.Text <- "Mostramos el menú."
